d4398218ea302596f24631410fc0538d8e54bd5c
Pijus Petkevičius <32205463+cezaris13@users.noreply.github.com>
2020-06-01 15:24:41 +0300

    Add files via upload
    cleaning

--- dwm/config.h
+++ dwm/config.h
@@ -115,15 +115,6 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioPrev,	spawn,		SHCMD("playerctl previous") },
 	{ 0, XF86XK_AudioNext,	spawn,		SHCMD("playerctl next") },
 // 	{ 0, XF86XK_Sleep,		spawn,		SHCMD("[ \"$(printf\No\\nYes\ | dmenu -i -sf white -nf gray -p \"Hibernate computer?\")\" = Yes ] && systemctl suspend") },
-    TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	TAGKEYS(                        XK_KP_End,                    0)
 	TAGKEYS(                        XK_KP_Down,                   1)
 	TAGKEYS(                        XK_KP_Page_Down,              2)
