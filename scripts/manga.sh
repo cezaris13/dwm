5ae9dd0494490562ecd20037bb14bdc85bf302a5
cezaris13 <pijus.cezar13@gmail.com>
2020-06-17 21:31:36 +0300

    fixed bugs

--- scripts/manga.sh
+++ scripts/manga.sh
@@ -1,3 +1,3 @@
-# wget https://toonily.com/
-echo "$(cat index.html | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | grep -v "^[/({},)/]\|}\?)\?;$\|Manhwa\|\-[0-9].\?[0-9]\?" | grep "-" | grep "\- \?[A-Z]" | grep -vE "[[:upper:]]{3}" | grep -A1 "\-[A-Z].*[A-Za-z].\?\-"| awk 'length > 2' |sed 's/.$//;s/^.//' |  awk 'NR%2{printf "%s ",$0;next;}1' | grep "Chapter" | sed "s/ \+/ /g;s/ $//g;s/&#8217;/'/g" | sort -u)" | dmenu -i -l 20 -p "new manga"
-# rm index.html
+wget  https://toonily.com/ #https://mangakakalot.com/
+echo "$(cat index.html | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | grep -v "^[/({},)/]\|}\?)\?;$\|Manhwa\|[Mm]anga\|\-[0-9].\?[0-9]\?\|^ " | grep "-" | grep "\- \?[A-Z]" | grep -vE "[[:upper:]]{3}" | sed "s/[0-9]:/& ccc/" | sed "s/ccc.*$//g" | grep -A1 "\-[A-Z].*[A-Za-z].\?\-"| awk 'length > 2' |sed 's/.$//;s/^.//' |  awk 'NR%2{printf "%s ",$0;next;}1' | grep "Chapter" | sed "s/ \+/ /g;s/ $//g;s/&#8217;/'/g;s/:$//g" | sort -u)" | dmenu -i -l 20 -p "new manga"
+ rm index.html
