1734c8a9fc9d08662a1395e8acd9a53e3e3bf456
cezaris13 <pijus.cezar13@gmail.com>
2020-12-25 21:47:16 +0200

    modified corona.sh to work

--- scripts/corona.sh
+++ scripts/corona.sh
@@ -1,5 +1,5 @@
 cd $HOME/.config/dwm/scripts/;
 wget --hsts-file $HOME/.cache/wget-hsts https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Lithuania ;
-echo "$(cat 2020_coronavirus_pandemic_in_Lithuania | grep ">Confirmed cases<" | sed "s/<[^>]*>/: /g" | awk '{ gsub(":", ":\n") } 1' | awk 'length > 2' | grep -v "^.[,()&]" | awk 'NR%2{printf "%s ",$0;next;}1' | grep -e "Confirmed cases:" -e "Deaths" | sed 's/.$//;s/^ //')" > 2020_coronavirus_pandemic_in_Lithuania
+echo "$(cat 2020_coronavirus_pandemic_in_Lithuania | grep ">Confirmed cases<" | sed "s/<[^>]*>/: /g" | awk '{ gsub(":", ":\n") } 1' | awk 'length > 2' | grep -A1 "Confirmed\|Deaths" | grep -v "-" | awk 'NR%2{printf "%s ",$0;next;}1' | sed "s/:$//g")" > 2020_coronavirus_pandemic_in_Lithuania
 cat 2020_coronavirus_pandemic_in_Lithuania | dmenu
 rm 2020_coronavirus_pandemic_in_Lithuania
