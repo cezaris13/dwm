91ba21b2574ef119535889f37ef5b3d1461b8f38
cezaris13 <pijus.cezar13@gmail.com>
2020-12-28 17:48:32 +0200

    optimisation

--- scripts/luke.sh
+++ scripts/luke.sh
@@ -2,6 +2,6 @@ if [ "$(printf No\\nYes | dmenu -i -sf white -nf gray -p "Check if there's a new
 then
     cd $HOME/.config/dwm/scripts/;
     notify-send "information is beeing processed..."; python luke.py
-    cat result | sed "s/<[^>]*>/;/g;s/ \+/ /g" |  awk '{ gsub(";", "\n") } 1' | awk 'length > 2' | tac | awk '/ago/{print $0; getline;getline;getline; print}' | tac | awk 'NR%2{printf "%s ",$0;next;}1' | grep -v "YouTube" | grep -v "[Ss]ecure login"| grep -m 1 "ago" | dmenu
+    cat result | sed "s/<[^>]*>/;/g;s/ \+/ /g" |  awk '{ gsub(";", "\n") } 1' | awk 'length > 2' | grep -B2 "ago" | grep -v "\-\-\|@Luke" | awk 'NR%2{printf "%s ",$0;next;}1' | head -n 1 | dmenu 
     rm result
 fi
--- scripts/manga.sh
+++ scripts/manga.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd $HOME/.config/dwm/scripts/
 curl "$(echo -e "https://toonily.com/webtoons/?m_orderby=latest\nhttps://mangakakalot.com/" | dmenu -p "choose the site")" > index.html
-result="$(echo "$(cat index.html | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | grep -v "[Mm]anga\|[Mm]anhwa\|^[/({},)/]\|}\?)\?;$\|\-[0-9].\?[0-9]\?\|^ " | grep "-" | grep "\- \?[A-Z]" | grep -vE "[[:upper:]]{3}" | sed "s/[0-9]:/& ccc/" | sed "s/ccc.*$//g" | grep -A1 "\-[A-Z].*[A-Za-z].\?\-"| awk 'length > 2' |sed 's/.$//;s/^.//' | awk 'NR%2{printf "%s ",$0;next;}1' | grep "Chapter" | sed "s/ \+/ /g;s/ $//g;s/&#8217;/'/g;s/:$//g")" | dmenu -i -l 20 -p "new manga")" #sort -u buvo kad abeceliskai suzeti
-result="$(echo $result | sed "s/[Cc]hapter.*//g" | sed "s/[A-Z]/\L&/g" | sed "s/[Yy]ou're//g;s/[Tt]he//g;s/[']s//g;s/ /-/g;s/[!,;']//g" | grep -o "[A-Za-z].*[A-Za-z]"  ) "
-link="$(cat index.html | grep -o " href=\"http.*\"" | grep "[Cc]hapter" | sed "s/\" title.*$//g;s/\/\".\+$//g;s/href=\"//g;s/\/\"$//g" | grep -m1 $result)"
+result="$(cat index.html | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | grep -v "^\-$" | awk 'length > 4' | grep "^-" | grep -v "[{}\:]" | grep "\- \?\+[A-Z]" | grep -B1 "Chapter" | grep -vA1 "Chapter"| grep -v "^--$" | sed "s/^-//g;s/-$//g" | awk 'NR%2{printf "%s ",$0;next;}1' | dmenu -i -l 20 -p "new manga")"
+result="$(echo $result | sed "s/[Cc]hapter.*//g" | sed "s/[A-Z]/\L&/g" | sed "s/[Yy]ou're//g;s/[Tt]he//g;s/[']s//g;s/ /-/g;s/[!,;']//g" | grep -o "[A-Za-z].*[A-Za-z]"  ) " #optimise later
+link="$(cat index.html | grep -o " href=\"http.*\"" | grep "[Cc]hapter" | sed "s/\" title.*$//g;s/\/\".\+$//g;s/href=\"//g;s/\/\"$//g" | grep -m1 $result)"#same goes here
 [ ${#result} != 1 ] && [ ${#link} != 0 ] && chromium "$link" # doesn't work with mangakakalot
-[ ${#result} != 1 ] && [ ${#link} == 0 ] && notify-send "cannot open manga"
+[ ${#result} != 1 ] && [ ${#link} == 0 ] && notify-send "cannot open manga"# remember what ${#link} does
 rm index.html
