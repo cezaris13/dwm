2291c82f53bbed8553036612c8070f952eb959d1
cezaris13 <pijus.cezar13@gmail.com>
2020-12-29 09:48:46 +0200

    gitignore script

---
+++ scripts/gitignore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dir="$(pwd)"
+cd $HOME/Desktop/gitignore
+ans="$(ls *.gitignore | sed "s/.gitignore//g" | dmenu -i -l 20 -p "choose your language for gitignore" )"
+ans="$(echo -n $ans | sed "s/$/.gitignore/g")"
+cp $ans $dir 
+cd $dir
+mv $ans .gitignore
