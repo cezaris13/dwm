e4205d3f2476dd18b537cb1f153736855d1e76bf
cezaris13 <pijus.cezar13@gmail.com>
2021-01-02 16:06:21 +0200

    toonily changed links, rewrite this script

--- scripts/manga.sh
+++ scripts/manga.sh
@@ -2,8 +2,11 @@
 cd $HOME/.config/dwm/scripts/
 curl "$(echo -e "https://toonily.com/webtoons/?m_orderby=latest\nhttps://mangakakalot.com/" | dmenu -p "choose the site")" > index.html #toonily does not work for now, maybe reboot will do the thing
 result="$(cat index.html | sed "s/>/>\n/g" | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | awk 'length > 4' | grep -B1 "Chapter "  | grep -vA1 "Chapter " | awk 'length > 4' | sed "s/\-//g" | awk 'NR%2{printf "%s ",$0;next;}1' | grep -v "^[0-9]" | dmenu -i -l 20 -p "new manga")"
-result="$(echo $result | sed "s/[Cc]hapter.*//g" | sed "s/[A-Z]/\L&/g" | sed "s/[Yy]ou're//g;s/[Tt]he//g;s/[']s//g;s/ /-/g;s/[!,;']//g" | grep -o "[A-Za-z].*[A-Za-z]"  ) " #optimise later
-link="$(cat index.html | grep -o " href=\"http.*\"" | grep "[Cc]hapter" | sed "s/\" title.*$//g;s/\/\".\+$//g;s/href=\"//g;s/\/\"$//g" | grep -m1 $result)"#same goes here
+result="$(echo $result | sed "s/[Cc]hapter.*//g" | sed "s/[A-Z]/\L&/g" | sed "s/[Yy]ou're//g;s/[Tt]he//g;s/[']s//g;s/ /-/g;s/[!,;']//g;s/\-\+/-/g" | grep -o "[A-Za-z].*[A-Za-z]"  ) " #optimise later
+link="$(cat index.html | sed "s/>/>\n/g" | grep -o " href=\"http.*\"" | grep "[Cc]hapter" | sed "s/\" title.*$//g;s/\/\".\+$//g;s/href=\"//g;s/\/\"$//g" | grep -m1 $result )"
+# echo $link
+echo ${#result}
+echo ${#link}
 [ ${#result} != 1 ] && [ ${#link} != 0 ] && chromium "$link" # doesn't work with mangakakalot
 [ ${#result} != 1 ] && [ ${#link} == 0 ] && notify-send "cannot open manga"# remember what ${#link} does
 rm index.html
