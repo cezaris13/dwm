55eb96b05471e3aea938f807cadb6173cf03eb68
cezaris13 <pijus.cezar13@gmail.com>
2021-01-03 15:29:55 +0200

    rewritten manga.sh

--- scripts/manga.sh
+++ scripts/manga.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd $HOME/.config/dwm/scripts/
 curl "$(echo -e "https://toonily.com/webtoons/?m_orderby=latest\nhttps://mangakakalot.com/" | dmenu -p "choose the site")" > index.html #toonily does not work for now, maybe reboot will do the thing
-result="$(cat index.html | sed "s/>/>\n/g" | sed "s/<[^>]*>/-/g;s/ \+/ /g;s/-\+/-/g" | awk 'length > 4' | grep -B1 "Chapter "  | grep -vA1 "Chapter " | awk 'length > 4' | sed "s/\-//g" | awk 'NR%2{printf "%s ",$0;next;}1' | grep -v "^[0-9]" | dmenu -i -l 20 -p "new manga")"
-result="$(echo $result | sed "s/[Cc]hapter.*//g" | sed "s/[A-Z]/\L&/g" | sed "s/[Yy]ou're//g;s/[Tt]he//g;s/[']s//g;s/ /-/g;s/[!,;']//g;s/\-\+/-/g" | grep -o "[A-Za-z].*[A-Za-z]"  ) " #optimise later
-link="$(cat index.html | sed "s/>/>\n/g" | grep -o " href=\"http.*\"" | grep "[Cc]hapter" | sed "s/\" title.*$//g;s/\/\".\+$//g;s/href=\"//g;s/\/\"$//g" | grep -m1 $result )"
-# echo $link
-echo ${#result}
-echo ${#link}
-[ ${#result} != 1 ] && [ ${#link} != 0 ] && chromium "$link" # doesn't work with mangakakalot
-[ ${#result} != 1 ] && [ ${#link} == 0 ] && notify-send "cannot open manga"# remember what ${#link} does
+cat index.html | sed "s/>/>\n/g" | grep "</a>\|<a href.*>" | grep -v  "^ *</a>$" | grep -A1 "href" | grep -vB1 "href" | grep -v "\-\-" | awk 'NR%2{printf "%s ",$0;next;}1'  | sed "s/<a \+href=.//g;s/<\/a>//g" | grep " *https\?://" > links_and_titles.txt
+result="$(cat links_and_titles.txt | grep -o ">.*$" | grep -B1 "Chapter" | sed "s/> *//g" | grep -vA1 "Chapter" |awk 'length >4'|  awk 'NR%2{printf "%s ",$0;next;}1' |  dmenu -i -l 20 -p "new manga")"
+result=$(echo $result | sed "s/Chapter.*$//g" | sed "s/^ \+//g" | sed "s/ \+$//g")
+link=$(cat links_and_titles.txt | grep -A1 "$result" | grep "Chapter" | sed 's/".*$//g;s/^ \+//g'| grep -m1 "https\?") 
+# todo vol for mangakakalot
+#3 seds into 1 line 6
+[ ${#result} != 1 ] && [ ${#link} != 0 ] && chromium "$link"
 rm index.html
+rm links_and_titles.txt
