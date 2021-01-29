00a5e4156e410f1abb04c99c976faaba1b6ff78b
cezaris13 <pijus.cezar13@gmail.com>
2021-01-29 17:09:15 +0200

    monitor orientation for my monitor

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -16,14 +16,14 @@ then
 #add your arandr layout script here
     if [[ "$(cat $FILE)" == "horizontal" ]]
     then 
-	    xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off 
+        xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
     elif [[ "$(cat $FILE)" == "vertical left" ]]
     then 
-	    xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+        xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate left --output VIRTUAL1 --off
     else
-        xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+        xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate right --output VIRTUAL1 --off
     fi
 elif [[ "$count" == "1" ]] 
-then 
+then
     xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
 fi
