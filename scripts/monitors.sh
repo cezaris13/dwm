60da92a69432496d759efb15c2ebbcdbdcbbda4d
cezaris13 <pijus.cezar13@gmail.com>
2021-06-05 15:49:31 +0300

    english exam

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -58,7 +58,9 @@ then
 #         xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #         feh --bg-scale $MonitorOne $MonitorTwoHorizontal 
         xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-        feh --bg-scale $MonitorTwoHorizontal 
+        feh --bg-scale $MonitorTwoHorizontal
+        # xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+        # feh --bg-scale $MonitorOne $MonitorTwoHorizontal
     elif [[ "$(cat $FILE)" == "vertical left" ]]
     then 
         xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate left --output VIRTUAL1 --off
