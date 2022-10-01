9e2fee3ab7d4fbb17ea2f4a722736808bc9ee42b
cezaris13 <pijus.cezar13@gmail.com>
2022-10-01 18:28:35 +0300

    refactoring

--- scripts/dwm_specific/brightness.sh
+++ scripts/dwm_specific/brightness.sh
@@ -1,1 +1,20 @@
-notify-send --hint=string:x-dunst-stack-tag:brightness "brightness: $(printf "%.0f\n" $(xbacklight -get)) %"
+#!/bin/bash
+
+function set_screen_brightness() {
+    option=$1
+    quiet=$2
+    brightness_sign=$(echo $option | head -c 1);
+    value="${option:1}"
+    if [[ "$brightness_sign" == "+" ]]
+    then
+        xbacklight -inc $value
+    else
+        xbacklight -dec $value
+    fi
+    if [ -z $quiet ]
+    then
+        notify-send --hint=string:x-dunst-stack-tag:brightness "brightness: $(printf "%.0f\n" $(xbacklight -get)) %"
+    fi
+}
+
+set_screen_brightness $1 $2
--- scripts/dwm_specific/volume/volume_percentage.sh
+++ scripts/dwm_specific/volume/volume_percentage.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-bluetooth_headphones=$(sh $HOME/.config/dwm/scripts/dwm_specific/bluetooth//./find_bluetooth_headphones.sh )
-if [[ ! -z $bluetooth_headphones ]]
-then
-    bluetooth_sink_mame=$(pacmd list-sinks | grep --color=never name:.*$ | grep --color=never "^.*bluez.*$" | sed "s/.*<//g;s/>//g");
-    pactl get-sink-volume $bluetooth_sink_mame | grep --color=never -o "front-right:.*" | grep -o "/.*/" | sed "s/\///g; s/ //g" | sed -e "s/\(.*\)/[\1]/";
-else
-    amixer get Master | awk '/Mono.+/ {print $6=="[off]"?$6:$4}';
-fi
+function get_volume_percentage() {
+    bluetooth_headphones=$(sh $HOME/.config/dwm/scripts/dwm_specific/bluetooth//./find_bluetooth_headphones.sh )
+    if [[ ! -z $bluetooth_headphones ]]
+    then
+        bluetooth_sink_mame=$(pacmd list-sinks | grep --color=never name:.*$ | grep --color=never "^.*bluez.*$" | sed "s/.*<//g;s/>//g");
+        pactl get-sink-volume $bluetooth_sink_mame | grep --color=never -o "front-right:.*" | grep -o "/.*/" | sed "s/\///g; s/ //g" | sed -e "s/\(.*\)/[\1]/";
+    else
+        amixer get Master | awk '/Mono.+/ {print $6=="[off]"?$6:$4}';
+    fi
+}
+
+get_volume_percentage
