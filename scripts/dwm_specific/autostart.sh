8d39129d6ed0a410e8378a3cfa6d1f4fa90c8c52
cezaris13 <pijus.cezar13@gmail.com>
2023-02-26 15:00:49 +0200

    dunst config location change

--- scripts/dwm_specific/autostart.sh
+++ scripts/dwm_specific/autostart.sh
@@ -8,7 +8,7 @@ setxkbmap -layout us,lt -option 'grp:alt_shift_toggle' & #sita reiks pakoreguoti
 $HOME/.config/dwm/scripts/dwm_specific/./keyboard.sh &
 xbindkeys &
 numlockx on &
-dunst &
+dunst -config $HOME/.config/dwm/configs/dunstrc &
 fusuma --config $HOME/.config/dwm/configs/fusuma.yml &
 xinput --set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1 # reverse scrolling
 xinput --set-prop "Elan Touchpad" "libinput Tapping Enabled" 1           #tap clicking
